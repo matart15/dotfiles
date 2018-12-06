2067c8ae2305f1b5c239a75a3708736d6605e055
matar <matart15@gmail.com>
2018-12-06 11:48:21 +0900

    node 10 path

--- zsh/zsh.after/path.zsh
+++ zsh/zsh.after/path.zsh
@@ -3,6 +3,7 @@ export PATH=/Users/matar/Library/Python/3.6/bin:$PATH
 export PATH=~/.composer/vendor/bin:$PATH
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+export PATH="/usr/local/opt/node@10/bin:$PATH"
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/matar/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/matar/Downloads/google-cloud-sdk/path.zsh.inc'; fi
